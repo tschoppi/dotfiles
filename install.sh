dcb38680ebe78dd74be8e6311c5dd81e339dda04
Jan Tschopp <jan.tschopp@sitrox.com>
2017-05-26 13:17:47 +0200

    Add dunst notification server

--- install.sh
+++ install.sh
@@ -9,10 +9,13 @@ if [ $# != 1 ]; then
     echo "Did you edit fehbg?"
 fi
 
+mkdir -p $HOME/.config/{i3,dunst}
+
 cp tmux.conf $HOME/.tmux.conf
 cp vimrc $HOME/.vimrc
 cp Xresources $HOME/.Xresources
 cp i3_config $HOME/.config/i3/config
+cp dunstrc $HOME/.config/dunst/dunstrc
 cp fehbg $HOME/.fehbg
 
 exit 0
